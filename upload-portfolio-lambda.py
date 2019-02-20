bb67ebf4b86fbd57d7f9a54871a38569156ec593
dhwang21 <hwang.derrick@gmail.com>
2019-02-19 23:33:06 -0800

    switching branch

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,12 +5,25 @@ import StringIO
 import zipfile 
 
 def lambda_handler(event, context):
+    location = {
+        "bucketName": 'portfoliobuild.derrickhwang.com',
+        "objectKey": 'portfoliobuild.zip'
+    }
+    
+    job = event.get("CodePipeline.job")
+    
+    if job:
+        for artifact in job["data"]["inputArtifcats"]:
+            if artifact["name"] == "portfolioBuild":
+                location = artifact["location"]["s3Location"]
+                
+                
     s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))                                                                  
     portfolio_bucket = s3.Bucket('portfolio.derrickhwang.com')
-    build_bucket = s3.Bucket('portfoliobuild.derrickhwang.com')                                
+    build_bucket = s3.Bucket(location["bucketName"])                                
       
     portfolio_zip = StringIO.StringIO()                                                        
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)                         
+    build_bucket.download_fileobj(location["objectKey"], portfolio_zip)                         
                                                                                 
                                            
     with zipfile.ZipFile(portfolio_zip) as myzip:                                              
@@ -23,4 +36,4 @@ def lambda_handler(event, context):
                 portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetype})
             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
             
-    return 'Hello from Lambda'                      
+    return 'Hello from Lambda'      
